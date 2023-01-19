a15311d2d47761a0e93f131e2ff21a5d15f685ad
Brandon Tiernay <btiernay@Brandons-MacBook-Air.local>
2023-01-19 11:36:18 -0500

    lab done, iterations 1-8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,69 @@
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
+  // Iteration 2
+  
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+
+    this.intervalId = setInterval(()=> {
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+       this.currentTime++
+      }, 1000)
+      
+    }
+
+    // Iteration 3
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
+
+
+  // Iteration 4
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
+
+
+  // Iteration 5
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (String(value).length === 1) {
+      return `0${value}`
+    } else {
+      return String(value);
+    }
   }
+
+
+  // Iteration 6
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
+
+
+  // Iteration 7
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
+
+  // Iteration 8
+
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${minutes}:${seconds}`
   }
+
 }
